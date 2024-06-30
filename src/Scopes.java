340421a9a1173c7ca3351f6d58182593ce85ac7f
PranayKatkar12 <katkarpranay221@gmail.com>
2024-06-30 22:49:28 +0530

    scopes

--- src/Scopes.java
+++ src/Scopes.java
@@ -1,5 +1,24 @@
+class Algebra{
+    int a=10;
+    int b=30;
+
+    int show(){
+        return a+b;
+    }
+
+    {
+        int p=10;
+        int q=30;
+        System.out.printf("%d\n%d",p,q);
+    }
+
+}
+
 public class Scopes {
     public static void main(String[] args) {
+       Algebra b=new Algebra();
+        System.out.println();
+        System.out.println("Show method:"+b.show());
 
     }
 }
